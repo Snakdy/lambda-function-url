9f6485aff170a8a60fd8ee81e99877121634768e
Django Cass <django@dcas.dev>
2024-04-17 22:29:25 +1000

    fixed incorrect payload handling

--- pkg/invoke/invoke.go
+++ pkg/invoke/invoke.go
@@ -21,7 +21,7 @@ func (svc *Service) Invoke(request any, response any, requestCtx RequestContext)
 	if err != nil {
 		return fmt.Errorf("converting request to json: %w", err)
 	}
-	
+
 	res, err := svc.InvokeJSON(payload, requestCtx)
 	if err != nil {
 		return err
@@ -36,15 +36,13 @@ func (svc *Service) Invoke(request any, response any, requestCtx RequestContext)
 }
 
 func (svc *Service) InvokeJSON(request []byte, requestCtx RequestContext) ([]byte, error) {
-	payload, err := json.Marshal(request)
-	if err != nil {
-		return nil, fmt.Errorf("converting request to json: %w", err)
-	}
 	// assemble the request
+	deadline := time.Now().Add(time.Second * time.Duration(svc.timeout))
 	req := messages.InvokeRequest{
-		Payload: payload,
+		Payload: request,
 		Deadline: messages.InvokeRequest_Timestamp{
-			Seconds: time.Now().Unix() + svc.timeout,
+			Seconds: deadline.Unix(),
+			Nanos:   int64(deadline.Nanosecond()),
 		},
 		RequestId:    requestCtx.RequestID,
 		XAmznTraceId: requestCtx.TraceID,
@@ -53,7 +51,7 @@ func (svc *Service) InvokeJSON(request []byte, requestCtx RequestContext) ([]byt
 	slog.Info("invoking function", "requestId", requestCtx.RequestID, "traceId", requestCtx.TraceID)
 	var res messages.InvokeResponse
 	// make the call
-	err = svc.rpcClient.Call("Function.Invoke", req, &res)
+	err := svc.rpcClient.Call("Function.Invoke", req, &res)
 	if err != nil {
 		return nil, fmt.Errorf("invoking function: %w", err)
 	}
